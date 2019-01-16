ff3f780458a0e9e05877b12048445599a1adba9c
felix-ma <1374147274@qq.com>
2019-01-17 00:09:56 +0800

    初始化项目

---
+++ src/main/java/cn/felix/studymybatiesplus/StudyMybatiesPlusApplication.java
@@ -0,0 +1,14 @@
+package cn.felix.studymybatiesplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMybatiesPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyMybatiesPlusApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/cn/felix/studymybatiesplus/StudyMybatiesPlusApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.felix.studymybatiesplus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudyMybatiesPlusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
